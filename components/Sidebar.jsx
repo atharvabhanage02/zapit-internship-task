8c3ed511e983f1a1cbbf3f590a73ab692c7fc21e
Atharva Bhanage <atharvabhanage2002@gmail.com>
2023-03-17 17:42:19 +0530

    refactor : listed categories

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { categories } from "@/data/categories";
 import {
   filterByCategory,
   sortHighToLow,
@@ -33,58 +34,18 @@ const Sidebar = () => {
         </div>
         <div className="">
           <p className="mt-2 mb-2 font-bold">Categories</p>
-          <label className="input-select flex items-center">
-            <input
-              type="radio"
-              name="SORT_BY_CATEGORIES"
-              onChange={() => dispatch(filterByCategory("stablecoins"))}
-            />
-            <p className="pl-2">Stable coins</p>
-          </label>
-          <label className="input-select flex items-center">
-            <input
-              type="radio"
-              name="SORT_BY_CATEGORIES"
-              onChange={() =>
-                dispatch(filterByCategory("smart_contract_platforms"))
-              }
-            />
-            <p className="pl-2">Smart Contract Platforms</p>
-          </label>
-          <label class="input-select flex">
-            <input
-              type="radio"
-              name="SORT_BY_CATEGORIES"
-              onChange={() =>
-                dispatch(dispatch(filterByCategory("asset_backed_tokens")))
-              }
-            />
-            <p className="pl-2">Asset backed tokens</p>
-          </label>
-          <label class="input-select flex">
-            <input
-              type="radio"
-              name="SORT_BY_CATEGORIES"
-              onChange={() => dispatch(dispatch(filterByCategory("cex")))}
-            />
-            <p className="pl-2">Cex</p>
-          </label>
-          <label class="input-select flex">
-            <input
-              type="radio"
-              name="SORT_BY_CATEGORIES"
-              onChange={() => dispatch(dispatch(filterByCategory("meme")))}
-            />
-            <p className="pl-2">Meme</p>
-          </label>
-          <label class="input-select flex">
-            <input
-              type="radio"
-              name="SORT_BY_CATEGORIES"
-              onChange={() => dispatch(dispatch(filterByCategory("wallets")))}
-            />
-            <p className="pl-2">Wallet</p>
-          </label>
+          {categories.map((category) => {
+            return (
+              <label className="input-select flex items-center">
+                <input
+                  type="radio"
+                  name="SORT_BY_CATEGORIES"
+                  onChange={() => dispatch(filterByCategory(category))}
+                />
+                <p className="pl-2">{category}</p>
+              </label>
+            );
+          })}
         </div>
       </div>
     </div>
