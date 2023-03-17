108fd652bdd2acf356fdbc66b0d5c8e981d3ac9e
Atharva Bhanage <atharvabhanage2002@gmail.com>
2023-03-17 17:45:51 +0530

    style : changed `footer` styling

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ import {
 } from "react-icons/ti";
 const Footer = () => {
   return (
-    <div className="flex py-8 border-t-2  justify-between items-center px-12 sm:flex sm:flex-col sm:items-center sm:mt-[18.5rem] sm:gap-8">
+    <div className="flex py-8 border-t-2  justify-between items-center px-12 sm:flex sm:flex-col sm:items-center  sm:gap-8">
       <img src="zapit_icon.svg" className="h-12 w-8" alt="Logo" />
       <div className="text-3xl flex gap-3 cursor-pointer">
         <TiSocialFacebookCircular />
